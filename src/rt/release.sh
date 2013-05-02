a2663fb93f3198b5e896306b07e26af3620bd505
Sebastian Schuberth <sschuberth@gmail.com>
2013-05-02 11:28:43 +0200

    Adjust patch paths to the new upstream Git repository layout
    The new upstream "msys-runtime" repository only contains the equivalent to
    what was previously available inside the /msys/rt/src/ directory of our
    CVS mirror, see
    
        http://repo.or.cz/w/msys.git/tree/b4ca4aaba1c45ee13cb52045aaf3d726f73e5e1d:/msys/rt/src
    
    So as there is only the runtime and no applications we need to drop the
    bash patch from here. If not contributed upstream in between, the bash
    patch should be added again as part of a new /src/bash/ directory.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- src/rt/release.sh
+++ src/rt/release.sh
@@ -7,27 +7,14 @@ die () {
 
 cd "$(dirname "$0")"
 
-target=
 debug=
-
-# see what to build
-case "$1" in
-	msys|bash)
-		target=$1
-		;;
-	*)
-		die "$0 (msys|bash) [options]"
-		;;
-esac
-
-shift
 
 test "a$1" = "a--debug" && debug=t
 debug_clean=
 test "$debug" = "$(cat debug.txt 2>/dev/null)" || debug_clean=t
 echo "$debug" > debug.txt
 
-test -d ../msys/msys/rt ||
+test -d ../msys ||
 (
 	cd ../.. &&
 	git submodule update --init src/msys
@@ -50,8 +37,6 @@ do
 	i=$(($i+1))
 done
 
-
-cd msys &&
 release=MSYS-g$(git show -s --pretty=%h HEAD) ||
 die "Could not detect MSYS release"
 
@@ -59,63 +44,21 @@ test -f /bin/cc.exe || ln gcc.exe /bin/cc.exe ||
 die "Could not make sure that MSys cc is found instead of MinGW one"
 
 # build msys.dll
-release_msys() {
-	(export MSYSTEM=MSYS &&
-	 export PATH=/bin:$PATH &&
-	 cd rt &&
-	 (test -d bld || mkdir bld) &&
-	 cd bld &&
-	 DLL=i686-pc-msys/winsup/cygwin/new-msys-1.0.dll &&
-	 (test -f Makefile && test -z "$debug_clean" ||
-	  ../src/configure --prefix=/usr) &&
-	 (test -z "$debug" || perl -i.bak -pe 's/-O2//g' $(find -name Makefile)) &&
-	 (test -z "$debug_clean" || make clean) &&
-	 (make || test -f $DLL) &&
-	 (test ! -z "$debug" || strip $DLL) &&
-	 rebase -b 0x68000000 $DLL &&
-	 mv $DLL /bin/) &&
-	cd / &&
-	hash=$(git hash-object -w bin/new-msys-1.0.dll) &&
-	git update-index --cacheinfo 100755 $hash bin/msys-1.0.dll &&
-	git commit -s -m "Updated msys-1.0.dll to $release" &&
-	/share/msysGit/post-checkout-hook HEAD^ HEAD 1
-}
-
-# build bash.exe
-release_bash() {
-	(export MSYSTEM=MSYS &&
-	 export PATH=/bin:$PATH &&
-	 cd packages/bash &&
-	 case "$(git config --get --bool core.autocrlf)" in
-	 true)
-		git config core.autocrlf false &&
-		git read-tree HEAD &&
-		git stash
-		;;
-	 esac &&
-	 (test -d bld || mkdir bld) &&
-	 cd bld &&
-	 BASH=bash.exe &&
-	 (test -f Makefile && test -z "$debug_clean" ||
-	  ../3.1/configure \
-		--prefix=/usr \
-		--sysconfdir=/etc \
-		--localstatedir=/var \
-		--disable-nls \
-		--disable-rpath \
-		--without-curses) &&
-	 (test -z "$debug" || perl -i.bak -pe 's/-O2//g' $(find -name Makefile)) &&
-	 (test -z "$debug_clean" || make clean) &&
-	 make &&
-	 test -f $BASH &&
-	 (test ! -z "$debug" || strip $BASH) &&
-	 cp $BASH /bin/new-bash.exe) &&
-	cd / &&
-	hash=$(git hash-object -w bin/new-bash.exe) &&
-	git update-index --cacheinfo 100755 $hash bin/bash.exe &&
-	git update-index --cacheinfo 100755 $hash bin/sh.exe &&
-	git commit -s -m "Updated bash.exe to $release" &&
-	/share/msysGit/post-checkout-hook HEAD^ HEAD 1
-}
-
-release_$target
+(export MSYSTEM=MSYS &&
+ export PATH=/bin:$PATH &&
+ (test -d bld || mkdir bld) &&
+ cd bld &&
+ DLL=i686-pc-msys/winsup/cygwin/new-msys-1.0.dll &&
+ (test -f Makefile && test -z "$debug_clean" ||
+  ../configure --prefix=/usr) &&
+ (test -z "$debug" || perl -i.bak -pe 's/-O2//g' $(find -name Makefile)) &&
+ (test -z "$debug_clean" || make clean) &&
+ (make || test -f $DLL) &&
+ (test ! -z "$debug" || strip $DLL) &&
+ rebase -b 0x68000000 $DLL &&
+ mv $DLL /bin/) &&
+cd / &&
+hash=$(git hash-object -w bin/new-msys-1.0.dll) &&
+git update-index --cacheinfo 100755 $hash bin/msys-1.0.dll &&
+git commit -s -m "Updated msys-1.0.dll to $release" &&
+/share/msysGit/post-checkout-hook HEAD^ HEAD 1
